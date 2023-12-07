8619cf03008b1e70d86f58e0771ff747d8af71c9
Bradley Kreider <1699478+bkreider@users.noreply.github.com>
2023-12-07 13:09:06 -0500

    first commit

---
+++ recipe/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASE_URL="https://conda.anaconda.org"
+NAME=fake_services
+CHANNEL_NAME="services"
+URL=${BASE_URL}/${CHANNEL_NAME}
+
+for platform in win-64 linux-64 osx-64 noarch
+do 
+  
+  mkdir -p $PREFIX/$NAME/$platform
+
+  # without this file - conda is very slow on windows
+  echo "{}" >> $PREFIX/$NAME/$platform/current_repodata.json
+
+  curl -o $PREFIX/$NAME/$platform/repodata.json $URL/$platform/repodata.json
+  curl -o $PREFIX/$NAME/$platform/patch_instructions.json $URL/$platform/patch_instructions.json
+  curl -o $PREFIX/$NAME/$platform/repodata_from_packages.json $URL/$platform/repodata_from_packages.json
+done
+
+
